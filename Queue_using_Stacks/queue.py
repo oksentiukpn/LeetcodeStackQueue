5a08418bcb8bf21dea7dd23c4acda32aaede030f
oksentiukpn <oksentiuk.pn@ucu.edu.ua>
2026-04-03 11:06:56 +0300

    implemented queue using two stacks, where first stack is used for accepting elements and second stack is used for withdrawing. When second stack is empty, it uses elements from first stack.

---
+++ Queue_using_Stacks/queue.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+
+class MyQueue(object):
+    def __init__(self):
+        self.stack1 = deque()
+        self.stack2 = deque()
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack1.append(x)
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not self.stack1 and not self.stack2
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
