d219a865ee3e4f291fdb4a784880479562c6fa08
oksentiukpn <oksentiuk.pn@ucu.edu.ua>
2026-04-05 00:55:57 +0300

    rewrite Queue_using_Stacks using my own implementation of stack

--- Queue_using_Stacks/queue.py
+++ Queue_using_Stacks/queue.py
@@ -1,41 +1,77 @@
-from collections import deque
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, item):
+        self.head = Node(item, self.head)
+
+    def pop(self, i=0):
+        h = self.head
+        while i:
+            try:
+                h = h.next
+            except AttributeError:
+                raise IndexError("Or empty stack, or too high index")
+            i -= 1
+        try:
+            res = self.head.val
+            self.head = self.head.next
+        except AttributeError:
+            raise TypeError("Empty stack!")
+
+        return res
+
+    def peek(self):
+        try:
+            return self.head.val
+        except AttributeError:
+            raise TypeError("Empty stack!")
+
+    def is_empty(self):
+        return self.head is None
 
 
 class MyQueue(object):
     def __init__(self):
-        self.stack1 = deque()
-        self.stack2 = deque()
+        self.stack1 = Stack()
+        self.stack2 = Stack()
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
         """
-        self.stack1.append(x)
+        self.stack1.push(x)
 
     def pop(self):
         """
         :rtype: int
         """
-        if not self.stack2:
-            while self.stack1:
-                self.stack2.append(self.stack1.pop())
+        if self.stack2.is_empty():
+            while not self.stack1.is_empty():
+                self.stack2.push(self.stack1.pop())
         return self.stack2.pop()
 
     def peek(self):
         """
         :rtype: int
         """
-        if not self.stack2:
-            while self.stack1:
-                self.stack2.append(self.stack1.pop())
-        return self.stack2[-1]
+        if self.stack2.is_empty():
+            while not self.stack1.is_empty():
+                self.stack2.push(self.stack1.pop())
+        return self.stack2.peek()
 
     def empty(self):
         """
         :rtype: bool
         """
-        return not self.stack1 and not self.stack2
+        return self.stack1.is_empty() and self.stack2.is_empty()
 
 
 # Your MyQueue object will be instantiated and called as such:
@@ -44,3 +80,9 @@ class MyQueue(object):
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+q = MyQueue()
+q.push(1)
+q.push(2)
+print(q.peek())
+print(q.pop())
+print(MyQueue().empty())
